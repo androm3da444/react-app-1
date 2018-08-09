9dcaf6a47feebbe80cf5ddf8b9deadbd36705598
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-08-09 17:06:55 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -22,14 +22,49 @@ class App extends Component {
     })
   }
 
+  reloadWindow = (e) => {
+    window.location.reload()
+  }
+
+  nameChangedHandler = (e) => {
+    this.setState({
+      persons: [
+        { name: 'Ron', age: 27 },
+        { name: e.target.value, age: 31 },
+        { name: 'Drew', age: 37 }
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Welcome</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Testing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button
+          style={style}
+          onClick={this.switchNameHandler}>Switch Name</button>
+        <button
+          style={style}
+          onClick={this.reloadWindow}>Reload Window</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}
+          changed={this.nameChangedHandler} >Testing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
