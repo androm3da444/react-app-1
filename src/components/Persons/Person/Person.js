15665261e10526bc4d7f37649f436e869f6fc1f0
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-09-11 11:06:58 -0400

    added new component methods

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css'
 //import Radium from 'radium'
 
-const person = (props) => {
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[Person.js] Inside Constructor', props);
+    }
 
-    // const rnd = Math.random();
-    // if (rnd > 0.7) {
-    //     throw new Error('Something went wrong')
-    // }
+    componentWillMount() {
+        console.log('[Person.js] Inside componentWillMount()');
+    }
 
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
+    componentDidMount() {
+        console.log('[Person.js] Inside componentDidMount()');
+    }
+    render() {
+        console.log('[Person.js] Inside render()');
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        )
+    }
 }
 
-export default person;
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import classes from './App.css';
 //import Radium, { StyleRoot } from 'radium';
 import Persons from '../components/Persons/Persons';
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+//import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] Inside Constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount()');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate');
+  }
+
   state = {
     persons: [
       { id: 'asdf1', name: 'Ron', age: 27 },
@@ -61,6 +87,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] Inside render()');
 
     let persons = null;
 
